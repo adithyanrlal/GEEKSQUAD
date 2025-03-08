e55b1de8ff4deb4b59731e2058390b29e4c640f7
Robin-github-123 <151839372+Robin-github-123@users.noreply.github.com>
2025-03-08 13:16:13 +0530

    Consumer home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ConsumerHome from './components/Consumer_Home'
 
 function App() {
 
   return (
     <>
-
+    <ConsumerHome />
     </>
+    
   )
 }
 
